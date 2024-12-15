2b69ca16209e826d1ca546f1d82a0d79f33e3b74
Liam Tietjens <l.tietjens@hotmail.com>
2024-12-15 16:01:57 +1300

    have tried some stuff dioxus plus tailwind is the way to go

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -1,20 +1,23 @@
 use crate::Route;
 use dioxus::prelude::*;
 
-const NAVBAR_CSS: Asset = asset!("/assets/styling/navbar.css");
+// const NAVBAR_CSS: Asset = asset!("/assets/styling/navbar.css");
 
 #[component]
 pub fn Navbar() -> Element {
     rsx! {
-        document::Link { rel: "stylesheet", href: NAVBAR_CSS }
+        // document::Link { rel: "stylesheet", href: NAVBAR_CSS }
 
         div {
+            class: "rounded-b-md border-b-2 backdrop-blur-sm bg-white/30  sticky top-0 p-2 flex space-x-2",
             id: "navbar",
             Link {
+                class: "rounded-md border-2 m-1 p-0.5",
                 to: Route::Home {},
                 "Home"
             }
             Link {
+                class: "rounded-md border-2 m-1 p-0.5",
                 to: Route::Blog { id: 1 },
                 "Blog"
             }
--- src/views/blog.rs
+++ src/views/blog.rs
@@ -1,12 +1,12 @@
 use crate::Route;
 use dioxus::prelude::*;
 
-const BLOG_CSS: Asset = asset!("/assets/styling/blog.css");
+// const BLOG_CSS: Asset = asset!("/assets/styling/blog.css");
 
 #[component]
 pub fn Blog(id: i32) -> Element {
     rsx! {
-        document::Link { rel: "stylesheet", href: BLOG_CSS}
+        // document::Link { rel: "stylesheet", href: BLOG_CSS}
 
         div {
             id: "blog",
