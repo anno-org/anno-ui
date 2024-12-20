bc7ec3bea503ec22d321e518883d8a2ccc795aa6
Liam Tietjens <l.tietjens@hotmail.com>
2024-12-20 22:07:57 +1300

    remove bad styling

--- src/components/hero.rs
+++ src/components/hero.rs
@@ -6,15 +6,39 @@ const HEADER_SVG: Asset = asset!("/assets/header.svg");
 pub fn Hero() -> Element {
     rsx! {
         div {
-            id: "hero",
+            class: "grid grid-cols-1",
             img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            div { class: "flex flex-col justify-self-center",
+                a {
+                    class: "p-4 m-1",
+                    href: "https://dioxuslabs.com/learn/0.6/",
+                    "📚 Learn Dioxus"
+                }
+                a {
+                    class: "p-4 m-1",
+                    href: "https://dioxuslabs.com/awesome",
+                    "🚀 Awesome Dioxus"
+                }
+                a {
+                    class: "p-4 m-1",
+                    href: "https://github.com/dioxus-community/",
+                    "📡 Community Libraries"
+                }
+                a {
+                    class: "p-4 m-1",
+                    href: "https://github.com/DioxusLabs/sdk",
+                    "⚙️ Dioxus Development Kit"
+                }
+                a {
+                    class: "p-4 m-1",
+                    href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
+                    "💫 VSCode Extension"
+                }
+                a {
+                    class: "p-4 m-1",
+                    href: "https://discord.gg/XgGxMSkvUM",
+                    "👋 Community Discord"
+                }
             }
         }
     }
